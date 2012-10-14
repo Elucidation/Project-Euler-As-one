3f47ded0cb69616f9addeec2defad7af2b7dc1c1
Sameer Ansari <elucidation@gmail.com>
2012-10-14 04:40:38 -0400

    shorter

--- ProjectEuler_45.py
+++ ProjectEuler_45.py
@@ -4,21 +4,16 @@ hexa = lambda n: n*(2*n-1)
 
 cmax = 0
 
-nt,np,nh = 285,165,143 # n value to pass into functions
-t,p,h = 1,2,3 # placeholders
+# hex, pent, tri order
 
+# Initial numbers result in 40755
+n = [143,165,285] # n value to pass into functions
+
+t,p,h = 1,2,3 # placeholders
 # Find next number after 40755
-while not (t == p == h != 40755):
-	t,p,h = tri(nt), pent(np), hexa(nh)
-	if p <= t and p < h:
-		# pen smallest
-		np += 1
-	elif t <= p and t < h:
-		# tri smallest
-		nt += 1
-	else:
-		# hex smallest or all equal
-		nh += 1
+while not (t == p == h):
+	n[[h,p,t].index(min([h,p,t]))] += 1
+	t,p,h = [tri(n[2]), pent(n[1]), hexa(n[0])] # Preference for incrementing h before p before t (largest -> smallest)
 
 print "Next triangle number that is also pentagonal and hexagonal = %d" % t
 # Next triangle number that is also pentagonal and hexagonal =  1533776805
